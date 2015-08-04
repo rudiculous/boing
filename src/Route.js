a04724dff7c3bd8ce94ef86cee7145968e5101d0
RJ van der Weide <rj@rdcl.nl>
2015-08-04 20:15:52 +0200

    Moved getFunctionSignature to separate module.

--- src/Route.js
+++ src/Route.js
@@ -3,6 +3,7 @@
 const path = require('path');
 
 const escapeRE = require('escape-regexp');
+const getFunctionSignature = require('@rdcl/function-signature');
 
 const Boing = require('./Boing');
 const er = require('./errors');
@@ -109,7 +110,7 @@ class Route {
         }
 
         let args = [];
-        for (let key of _getFunctionSignature(fn)) {
+        for (let key of getFunctionSignature(fn)) {
             if (key === 'params') {
                 args.push(params);
             }
@@ -208,53 +209,6 @@ class Route {
         };
     }
 
-}
-
-
-function _getFunctionSignature(fn) {
-    let str = fn.toString();
-
-    // Strip comments.
-    str = str.replace(/\/\/[^\r\n]*|\/\*[\s\S]*?\*\//g, '');
-
-    let match = str.match(/\(([^)]*)\)/);
-
-    if (match) {
-        let sig = match[1].split(/\s*,\s*/);
-
-        if (sig.length === 1 && !sig[0]) {
-            sig.length = 0;
-        }
-
-        let signature = [];
-
-        for (let key of sig) {
-            // TODO: Handle rest parameters. The position of the rest
-            // parameter matters, so this requires some thought. The
-            // following signatures are all different:
-            // * function (...rest, foo, bar)
-            // * function (foo, ...rest, bar)
-            // * function (foo, bar, ...rest)
-            // For now, just ignore rest parameters...
-            if (key.indexOf('...') === 0) {
-                continue;
-            }
-
-            // If there is a default value defined, remove it from the
-            // key.
-            key = key.replace(/\s*=.*$/, '');
-
-            // TODO: Should we do something with destructuring
-            // assignments?
-
-            signature.push(key);
-        }
-
-        return signature;
-    }
-    else {
-        return [];
-    }
 }
 
 exports = module.exports = Route;
